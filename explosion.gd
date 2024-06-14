92f4e5cb605387be4c561d7f00e20d1e8715affc
Arthur <arthurrochadx@gmail.com>
2024-06-14 11:48:57 -0300

    adding custom animation player based on ticks to suport animation rollback

--- explosion.gd
+++ explosion.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
 @export var despawn_timer: NetworkTimer
+@export var animation_player: NetworkAnimationPlayer
 
 func _network_spawn(data: Dictionary) -> void:
 	global_position = data['position']
 	despawn_timer.start()
+	animation_player.play("Explode")
 	
 func _network_despawn() -> void:
 	pass
